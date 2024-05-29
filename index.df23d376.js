064a35a6b3933efab54538f8dc436cb9cbc7b5c0
Hanna <hmatviienko@gmail.com>
2024-05-29 12:57:02 +0300

    make build

---
+++ index.df23d376.js
@@ -0,0 +1,2 @@
+const e=document.querySelector('a[href="#navigation"]'),c=document.documentElement;e.addEventListener("click",()=>{c.classList.add("no-scroll")});const n=document.querySelectorAll(".header__cover--top--wrap--nav--links");n?.forEach(e=>{e.addEventListener("click",function(){c.classList.remove("no-scroll")})});
+//# sourceMappingURL=index.df23d376.js.map
